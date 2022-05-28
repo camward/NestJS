503a1d3648cc4ec6560acb48a1e67365decfbcbf
alex <camward@yandex.ru>
2022-05-28 11:31:21 +0400

    подключение MongoDB npm i @typegoose/typegoose mongoose nestjs-typegoose npm i -D @types/mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,18 @@ import { AuthModule } from './auth/auth.module';
 import { ProductModule } from './product/product.module';
 import { TopPageModule } from './top-page/top-page.module';
 import { ReviewModule } from './review/review.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { getMongoConfig } from './configs/mongo.config';
 
 @Module({
 	imports: [
 		ConfigModule.forRoot(),
+		TypegooseModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getMongoConfig,
+		}),
 		AuthModule,
 		ProductModule,
 		TopPageModule,
