1ecffac2deb26c8c6688773330c94bef90bf8d10
alex <camward@yandex.ru>
2022-05-28 12:14:13 +0400

    сервис отзывов nest g service review

--- src/review/review.model.ts
+++ src/review/review.model.ts
@@ -1,7 +1,21 @@
-export class ReviewModel {
+import { prop } from '@typegoose/typegoose';
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
+import { Types } from 'mongoose';
+
+export interface ReviewModel extends Base {}
+export class ReviewModel extends TimeStamps {
+	@prop()
 	name: string;
+
+	@prop()
 	title: string;
+
+	@prop()
 	description: string;
+
+	@prop()
 	rating: number;
-	date: Date;
+
+	@prop()
+	productId: Types.ObjectId;
 }
