1a4404a7ee546f34a02109362507dc9fd09a3bbe
alex <camward@yandex.ru>
2022-05-31 21:11:54 +0400

    авторизация и генерация JWT npm i @nestjs/jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
 import { TypegooseModule } from 'nestjs-typegoose';
+import { AuthController } from './auth.controller';
 import { UserModel } from './user.model';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { getJWTConfig } from '../configs/jwt.config';
 
 @Module({
 	controllers: [AuthController],
@@ -15,6 +18,12 @@ import { AuthService } from './auth.service';
 				},
 			},
 		]),
+		ConfigModule,
+		JwtModule.registerAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getJWTConfig,
+		}),
 	],
 	providers: [AuthService],
 })
