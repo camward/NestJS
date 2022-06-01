5ac1bc973ea33f92875a7586064a7c88e7ca0bc3
alex <camward@yandex.ru>
2022-06-01 22:12:53 +0400

    агрегации в MongoDB nest g service product

---
+++ src/product/product.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@nestjs/common';
+import { ModelType } from '@typegoose/typegoose/lib/types';
+import { InjectModel } from 'nestjs-typegoose';
+import { FindProductDto } from './dto/find-product.dto';
+import { ProductModel } from './product.model';
+
+@Injectable()
+export class ProductService {
+	constructor(
+		@InjectModel(ProductModel)
+		private readonly productModel: ModelType<ProductModel>,
+	) {}
+
+	async findWithReviews(dto: FindProductDto) {
+		return this.productModel
+			.aggregate([
+				{
+					$match: {
+						categories: dto.category,
+					},
+				},
+				{
+					$sort: {
+						_id: 1,
+					},
+				},
+				{
+					$limit: dto.limit,
+				},
+				{
+					$lookup: {
+						from: 'Review',
+						localField: '_id',
+						foreignField: 'productId',
+						as: 'reviews',
+					},
+				},
+				{
+					$addFields: {
+						reviewCount: { $size: '$reviews' },
+						reviewAvg: { $avg: '$reviews.rating' },
+					},
+				},
+			])
+			.exec();
+	}
+}
