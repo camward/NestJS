53c10086ae8c11a571c00d35a2030d48af085f78
alex <camward@yandex.ru>
2022-06-03 21:37:46 +0400

    конвертация изображений npm i sharp npm i -D @types/sharp

--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -3,10 +3,12 @@ import { FileElementResponse } from './dto/file-element.reposonse';
 import { format } from 'date-fns';
 import { path } from 'app-root-path';
 import { ensureDir, writeFile } from 'fs-extra';
+import * as sharp from 'sharp';
+import { MFile } from './mfile.class';
 
 @Injectable()
 export class FilesService {
-	async saveFiles(files: Express.Multer.File[]): Promise<FileElementResponse[]> {
+	async saveFiles(files: MFile[]): Promise<FileElementResponse[]> {
 		const dateFolder = format(new Date(), 'yyyy-MM-dd');
 		const uploadFolder = `${path}/uploads/${dateFolder}`;
 		await ensureDir(uploadFolder);
@@ -19,5 +21,9 @@ export class FilesService {
 			});
 		}
 		return res;
+	}
+
+	convertToWebP(file: Buffer): Promise<Buffer> {
+		return sharp(file).webp().toBuffer();
 	}
 }
