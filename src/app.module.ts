1b06d13a69360066671f3341028adf130d1a4620
alex <camward@yandex.ru>
2022-06-04 12:48:30 +0400

    интеграция с Telegram nest g module telegram nest g service telegram npm i telegraf

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,8 @@ import { ReviewModule } from './review/review.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypegooseModule } from 'nestjs-typegoose';
 import { FilesModule } from './files/files.module';
+import { TelegramModule } from './telegram/telegram.module';
+import { getTelegramConfig } from './configs/telegram.config';
 import { getMongoConfig } from './configs/mongo.config';
 
 @Module({
@@ -23,6 +25,11 @@ import { getMongoConfig } from './configs/mongo.config';
 		TopPageModule,
 		ReviewModule,
 		FilesModule,
+		TelegramModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getTelegramConfig,
+		}),
 	],
 	controllers: [AppController],
 	providers: [AppService],
