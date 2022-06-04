7e7562c1ceeef439aac8761263e73c714974ba60
alex <camward@yandex.ru>
2022-06-04 13:32:10 +0400

    Schedule Module. Cron npm i @nestjs/schedule npm i -D @types/cron

--- src/top-page/top-page.controller.ts
+++ src/top-page/top-page.controller.ts
@@ -1,4 +1,16 @@
 import { Controller } from '@nestjs/common';
+import { Cron, CronExpression, SchedulerRegistry } from '@nestjs/schedule';
+import { TopPageService } from './top-page.service';
 
 @Controller('top-page')
-export class TopPageController {}
+export class TopPageController {
+	constructor(
+		private readonly topPageService: TopPageService,
+		private readonly scheduleRegistry: SchedulerRegistry,
+	) {}
+
+	@Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
+	async test() {
+		console.log('Cron');
+	}
+}
